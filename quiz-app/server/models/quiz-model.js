5b39dc223810f0b48cc07fb1daec4c2f47767556
Jonathan Lipman <jlipman500@gmail.com>
2021-01-30 15:37:04 -0600

    remove comments

--- quiz-app/server/models/quiz-model.js
+++ quiz-app/server/models/quiz-model.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Movie = new Schema(
+const Quiz = new Schema(
+    {
+        name: { type: String, required: true },
+        time: { type: [String], required: true },
+        rating: { type: Number, required: true },
+    },
+    { timestamps: true },
+)
+
+const Response = new Schema(
     {
         name: { type: String, required: true },
         time: { type: [String], required: true },
