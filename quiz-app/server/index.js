b7e8c6bbcd85be456a3061759af95b94aa42e247
Jonathan Lipman <jlipman500@gmail.com>
2021-03-11 17:18:55 -0600

    adding run on backend

--- quiz-app/server/index.js
+++ quiz-app/server/index.js
@@ -9,23 +9,27 @@ const swaggerFile = require('./swagger_output.json')
 const db = require('./db')
 const quizRouter = require('./routes/quiz-router')
 
-const app = express()
+const serverApp = express()
 const apiPort = 3000
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(uest())
-app.use(bodyParser.json())
+serverApp.use(bodyParser.urlencoded({ extended: true }))
+serverApp.use(cors())
+serverApp.use(uest())
+serverApp.use(bodyParser.json())
 
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const frontEndApp = express()
+frontEndApp.use(express.static(path.join(__dirname, 'build')))
+
+frontEndApp.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+frontEndApp.listen(8000)
 
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use('/api', quizRouter)
+serverApp.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+serverApp.use('/api', quizRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+serverApp.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
